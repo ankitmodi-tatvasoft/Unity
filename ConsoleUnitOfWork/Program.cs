ca41fa9a27cea174402bc8b5a38dd91aec6e6779
ankit <isdsupport@asg.com.au>
2018-04-12 10:18:55 +0530

    Add project files.

---
+++ ConsoleUnitOfWork/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitOfWorkExample;
+
+namespace ConsoleUnitOfWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer();
+            obj.Id = 1000;
+            obj.CustomerName = "shiv";
+            SimpleExampleUOW<Customer> o1 = new SimpleExampleUOW<Customer>();
+            o1.Add(obj);
+            obj = new Customer();
+            obj.Id = 2000;
+            obj.CustomerName = "xxxx";
+            o1.Add(obj);
+            o1.Committ();
+
+            SimpleExampleUOW<Supplier> o2 = new SimpleExampleUOW<Supplier>();
+            o2.Add(new Supplier());
+            o2.Committ();
+        }
+    }
+}
---
+++ UnitOfWorkExample/DAL/DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace UnitOfWorkExample.DAL
+{
+    public class DataAccess
+    {
+        private string ConnectionString = @"Data Source=PCI186\SQL2014;Initial Catalog=ankitTemp;Integrated Security=True";
+        public bool InsertCustomer(int CustomerCode,
+                                  string CustomerName)
+        {
+            SqlConnection objConn = new SqlConnection(ConnectionString);
+            try
+            {
+
+                objConn.Open();
+                string str = "insert into Customer values(" + CustomerCode + ",'"
+                                                           + CustomerName + "')";
+                SqlCommand objCommand = new SqlCommand(str, objConn);
+                objCommand.ExecuteNonQuery();
+                objConn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return true;
+        }
+
+        public SqlDataReader GetCustomer(int CustomerCode)
+        {
+            SqlConnection objConn = new SqlConnection(ConnectionString);
+            objConn.Open();
+            string str = "select * from Customer where CustomerCode=" + CustomerCode;
+            SqlCommand objCommand = new SqlCommand(str, objConn);
+            return objCommand.ExecuteReader();
+        }
+
+        public bool UpdateCustomer(int CustomerCode,
+                                   string CustomerName)
+        {
+            SqlConnection objConn = new SqlConnection(ConnectionString);
+            objConn.Open();
+            string str = "Update  Customer set CustomerName='"
+                            + CustomerName +
+                            "' where CustomerCode=" + CustomerCode;
+            SqlCommand objCommand = new SqlCommand(str, objConn);
+            objCommand.ExecuteNonQuery();
+            objConn.Close();
+            return true;
+        }
+    }
+}
